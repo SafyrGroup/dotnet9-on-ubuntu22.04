fb2a79760ad48138fc4a3be6d0c9751719b0eba9
SafyrGroup <github.com@safyr.co.uk>
2024-12-26 20:16:25 +0000

    Add script comments

--- install-dotnet9.sh
+++ install-dotnet9.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-apt update
+# add the backport repo which contains the aspnetcore-runtime-9.0 package, which will be used later to find and install dependancies
 add-apt-repository -y ppa:dotnet/backports
+# Update the pat database, including the new repo
 apt update
+# Install the default dotnet-host (version 8, as of the time of writing) and tolls used in subsequent commands
 apt install -y dotnet-host apt-rdepends wget grep gawk findutils
+# Find all dependancies of aspnercore-runtime-9.0, exepts for debconf (which doesn't directly install) and *dotnet* packages which would cause the removal of dotnet 8 and 7, and install them
 apt-rdepends -p aspnetcore-runtime-9.0 | grep NotInstalled | grep -E -v "(dotnet)|(debconf)" | awk '/Depends:/{print$2}' | xargs apt install
+# Get the offical install script and execute it, passing in the directory where dotnet-host is located, ensuring seemless integration and system-wide availability
 wget -O - https://dot.net/v1/dotnet-install.sh | bash -s - --channel STS --runtime aspnetcore --install-dir $(dirname $(realpath $(which dotnet)))
