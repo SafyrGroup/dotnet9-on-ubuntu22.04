2c7bfc17c0ad3248dda5b7fd77f2964c62a685d3
qumulus-yedidya <yedidya@qumulus.io>
2025-01-14 20:47:07 +0000

    Update install-dotnet9.sh

--- install-dotnet9.sh
+++ install-dotnet9.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+RUNTIME=dotnet #aspnetcore
+CHANNEL="9.0"
+while getopts "r:c:sh" opt; do
+  case $opt in
+    r) RUNTIME=$OPTARG ;;
+    c) CHANNEL=$OPTARG ;;
+    s) SDK=true ;;
+    h) show_help; exit 1 ;;
+    *) echo "Invalid option '$opt'"; exit 1 ;;
+  esac
+done
+
 # Add the backport repo, which contains the aspnetcore-runtime-9.0 package, which will be used later to find and install dependancies
 add-apt-repository -y ppa:dotnet/backports
 # Update the apt database, including the new repo
 apt update
 # Install the default dotnet-host (version 8, as of the time of writing) and tools used in subsequent commands
-apt install -y dotnet-host apt-rdepends wget grep gawk findutils
+apt install -y apt-rdepends wget grep gawk findutils
 # Find all dependancies of aspnercore-runtime-9.0, excepts for debconf (which doesn't directly install) and *dotnet* packages which would cause the removal of dotnet 8 and 7, and install them
-apt-rdepends -p aspnetcore-runtime-9.0 | grep NotInstalled | grep -E -v "(dotnet)|(debconf)" | awk '/Depends:/{print$2}' | xargs apt install
+apt-rdepends -p $RUNTIME-runtime-$CHANNEL | grep NotInstalled | grep -E -v "(dotnet)|(debconf)" | awk '/Depends:/{print$2}' | xargs apt install
 # Get the offical install script and execute it, passing in the directory where dotnet-host is located, ensuring seemless integration and system-wide availability
-wget -O - https://dot.net/v1/dotnet-install.sh | bash -s - --channel STS --runtime aspnetcore --install-dir $(dirname $(realpath $(which dotnet)))
+
+if [ "$SDK" -ne true ]; then
+  RUNTIME="--runtime $RUNTIME"
+else
+  RUNTIME=
+fi
+wget -O - https://dot.net/v1/dotnet-install.sh | bash -s - --channel $CHANNEL $RUNTIME --install-dir $(dirname $(realpath $(which dotnet)))
